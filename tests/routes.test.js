1f5960a23e9500d475271d0c897c8624ba61b553
Lina Qaddah <Lina.Qaddah@gmail.com>
2020-02-11 14:01:58 +0200

    setting up routes.js file @linaQh95 closes #10

---
+++ tests/routes.test.js
@@ -0,0 +1,15 @@
+const test = require('tape');
+const supertest = require('supertest');
+const router =require('../src/routes.js');
+
+test('Home route should work', t=>{
+    supertest(router)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end ((err,res)=>{
+            t.error(err)
+            t.equal(res.type,'text/html','Response should return the index.html type');
+            t.end();
+    })
+})
